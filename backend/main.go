c093a048d52b04148e39809d91a30f1661b9306b
You-Keitou <youkeitou327@gmail.com>
2023-10-06 04:16:15 +0900

    cors設定、ORM導入

--- backend/main.go
+++ backend/main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
 	//Ginフレームワークのデフォルトの設定を使用してルータを作成
 	router := gin.Default()
 
+	// create CORS config
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"}
+	router.Use(cors.New(config))
+
 	// ルートハンドラの定義
-	router.GET("/", func(c *gin.Context) {
+	router.GET("/api/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello, World!",
 		})

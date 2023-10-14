397758d134b862fe9347ae8fa9e0851bb16a1dbe
Taishiyy <taishi.y.mj@gmail.com>
2023-10-14 19:16:33 +0900

    belong to や many to many のアソシエーションの修正　User を削除

--- backend/models/event.go
+++ backend/models/event.go
@@ -1,12 +1,13 @@
 package models
 
 type Event struct {
-
-	EventId				int64 		`gorm:"primaryKey;not null" json:"event_id"`
-	User 				User 		`gorm:"foreignKey:UserId" json:"user_id"`
-	Category 			Category 	`gorm:"foreignKey:CategoryId" json:"category_id"`
-	Language 			[]Language 	`gorm:"foreignKey:LanguageId" json:"language_id"`
-	Tag 				[]Tag 		`gorm:"foreignKey:TagId" json:"tag_id"`
+	EventId				int64 		`gorm:"primaryKey;not null;" json:"event_id"`
+	UserId				int64 		`json:"user_id"`
+	User 				User 		`gorm:"foreignKey:UserId;"`
+	CategoryId			int64		`json:"category_id"`
+	Category 			Category 	`gorm:"foreignKey:CategoryId;"`
+	Languages 			[]Language 	`gorm:"many2many:event_languages;" json:"languages"`
+	Tags 				[]Tag 		`gorm:"many2many:event_tags;" json:"tags"`
 	EventImage 			string  	`json:"event_image"`
 	EventTitle 			string		`json:"event_title"`
 	EventDescription 	string		`json:"event_description"`
@@ -18,21 +19,16 @@ type Event struct {
 }
 
 type Category struct{
-	CategoryId 		int64	`gorm:"primaryKey;not null" json:"category_id"`
+	CategoryId 		int64	`gorm:"primaryKey;not null;" json:"category_id"`
 	CategoryName 	string	`json:"category_name"`
 }
 
 type Language struct {
-	LanguageId 		int64	`gorm:"primaryKey;not null" json:"language_id"`
+	LanguageId 		int64	`gorm:"primaryKey;not null;" json:"language_id"`
 	LanguageText 	string	`json:"language_text"`
 }
 
 type Tag struct {
-	TagId 	int64 	`gorm:"primaryKey;not null" json:"tag_id"`
+	TagId 	int64 	`gorm:"primaryKey;not null;" json:"tag_id"`
 	TagText string	`json:"tag_text"`
-}
-
-// 仮作成
-type User struct {
-	UserId int64	`gorm:"primaryKey;" json:"user_user"`
 }

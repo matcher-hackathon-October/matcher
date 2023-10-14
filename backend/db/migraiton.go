83e28518b5999fe2af145fca1bb14fd018d9a7b1
You-Keitou <youkeitou327@gmail.com>
2023-10-15 01:02:34 +0900

    migrationの簡単なツール

--- backend/db/migraiton.go
+++ backend/db/migraiton.go
@@ -1,15 +1,65 @@
-package migration
+package database
 
 import (
-	"flag"
-	"fmt"
-	"os"
 	"log"
-	"time"
-
-	- "github.com/go-sql-driver/postgres"
-	"github.com/golang-migrate/migrate/v4"
-	- "github.com/golang-migrate/migrate/v4/database/postgres"
-	- "github.com/golang-migrate/migrate/v4/source/file"
-	- "github.com/jinzhu/gorm"
+	"matcher_api/models"
+	"gorm.io/gorm"
 )
+
+func Migrate(db *gorm.DB) error {
+	if err := db.AutoMigrate(
+		&models.User{},
+		&models.UserProfile{},
+		&models.Notification{},
+		&models.NotificationType{},
+		&models.Event{},
+		&models.Category{},
+		&models.Language{},
+		&models.Tag{},
+	); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Drop(db *gorm.DB) error {
+	if err := db.Migrator().DropTable(
+		&models.User{},
+		&models.UserProfile{},
+		&models.Notification{},
+		&models.NotificationType{},
+		&models.Event{},
+		&models.Category{},
+		&models.Language{},
+		&models.Tag{},
+	); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Create(db *gorm.DB) error {
+	if err := db.Migrator().CreateTable(
+		&models.User{},
+		&models.UserProfile{},
+		&models.Notification{},
+		&models.NotificationType{},
+		&models.Event{},
+		&models.Category{},
+		&models.Language{},
+		&models.Tag{},
+	); err != nil {
+		return err
+	}
+	return nil
+}
+
+func Reset(db *gorm.DB) error {
+	if err := Drop(db); err != nil {
+		return err
+	}
+	if err := Create(db); err != nil {
+		return err
+	}
+	return Migrate(db)
+}

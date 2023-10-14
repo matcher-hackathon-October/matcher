0af705edb5a08e9e251a8f84356bd895c587ebd9
satoshi <dev.satoshi.1016@gmail.com>
2023-10-14 13:39:16 +0900

    add: CORSの設定を追加

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"matcher_api/config"
+	"matcher_api/middleware"
 	"matcher_api/routes"
 )
 
 func main() {
 	// ルーターのセットアップ
 	r := routes.SetupRouter()
+
+	// CORSミドルウェアを適用
+	r.Use(middleware.CORSMiddleware())
 
 	// サーバー起動
 	r.Run(config.Port)

2ea36ac6c08b5098cb12c7b99c35432c2a5aeefc
Taishiyy <taishi.y.mj@gmail.com>
2023-10-15 10:51:52 +0900

    language category tag のコントローラーを作成

---
+++ backend/controllers/language_controller.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"matcher_api/models"
+	"matcher_api/db"
+	"github.com/gin-gonic/gin"
+)
+
+// 全ての言語情報を取得
+func GetAllLanguages(c *gin.Context) {
+	var languages []models.Language
+
+	if err := database.DB.Find(&languages).Error; err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	// レスポンスをJSONとして返す
+	c.JSON(200, gin.H{"events": languages})
+}
---
+++ backend/controllers/tag_contoroller.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"matcher_api/models"
+	"matcher_api/db"
+	"github.com/gin-gonic/gin"
+)
+
+// 全てのtag情報を取得
+func GetAllTags(c *gin.Context) {
+	var tags []models.Tag
+
+	if err := database.DB.Find(&tags).Error; err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	// レスポンスをJSONとして返す
+	c.JSON(200, gin.H{"events": tags})
+}

46ff0a54bedf7f2699ecb19a219fba13890b07bf
Taishiyy <taishi.y.mj@gmail.com>
2023-10-15 14:42:57 +0900

    route.goにevent周りのコントローラーを追加

--- backend/routes/route.go
+++ backend/routes/route.go
@@ -16,5 +16,24 @@ func SetupRouter() *gin.Engine {
 	r.DELETE("/users/:id", controllers.DeleteUser)
 	r.PATCH("/users/:id/profile", controllers.UpdateUserProfile)
 
+	// event
+	r.GET("/events", controllers.CreateEvent)
+	r.GET("/events/:event_id", controllers.GetEventDetails)
+	r.POST("/events", controllers.CreateEvent)
+	r.PUT("/events/:event_id", controllers.UpdateEvent)
+	r.DELETE("/events/:event_id", controllers.DeleteEvent)
+
+	// language
+	r.GET("/languages", controllers.GetAllLanguages)
+	r.GET("/languages/:language_id", controllers.GetLanguageDetails)
+
+	// tag
+	r.GET("/tags", controllers.GetAllTags)
+	r.GET("/tags/:tag_id", controllers.GetTagDetails)
+
+	// category
+	r.GET("/categories", controllers.GetAllCategories)
+	r.GET("/categories/:category_id", controllers.GetCategoryDetails)
+
 	return r
 }
